dc4084e8cd7c5213972cef0affbb582bc56e2886
Shivam Singh <singh-shivam@outlook.com>
2023-06-10 19:31:23 +0530

    Added Page Title Change

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,21 @@ import { useState, useEffect } from 'react';
 import { DemoService } from './api/Service';
 
 function App() {
+
   const [buttonText, setButtonText] = useState("Not Here");
+
+  useEffect(() => {
+    document.title = 'My Project';
+  }, []);
+
   useEffect(() => {
     const getData = async () => {
       const response = await DemoService.test();
       setButtonText(response.data);
     }
     getData();
-  }, [])
+  }, []);
+
   
   return (
     <Button variant='contained' size='small' onClick={() => alert('You clicked a button.')}>{buttonText}</Button>
